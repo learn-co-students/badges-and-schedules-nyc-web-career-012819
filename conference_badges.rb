2f4addfc1c9c61f64da01d070f701fc155d8d39c
github username <github email address>
2019-01-16 17:58:07 -0500

    created messages

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_messages = []
+  badge_messages = array.collect{|name| badge_maker(name)}
+end
+
+def assign_rooms(speakers)
+  new_arr = []
+  num = 1
+  speakers.each do |speaker|
+    if num < 8
+      message = "Hello, #{speaker}! You'll be assigned to room #{num}!"
+      new_arr = new_arr.push(message)
+      num += 1
+    else
+      break
+    end
+  end
+  new_arr
+end
+
+def printer(array)
+  arr_badge = batch_badge_creator(array)
+  arr_room = assign_rooms(array)
+  arr_badge.each do |message|
+    puts message
+  end
+  arr_room.each do |room|
+    puts room
+ end
+end
